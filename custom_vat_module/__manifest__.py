744fabcbed03b7712ad4e4f9e9d047a427e0b16e
djema32 <159773360+djema32@users.noreply.github.com>
2025-01-14 20:09:39 +0100

    Update __manifest__.py

--- custom_vat_module/__manifest__.py
+++ custom_vat_module/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Custom VAT Module',
-    'version': '1.0',
+    'version': '1.0.0.1',
     'summary': 'Show product price excluding VAT alongside the including VAT price.',
     'author': 'Xander Djema',
     'category': 'Website & eCommerce',
@@ -12,4 +12,9 @@
     ],
     'installable': True,
     'application': False,
+    'license': 'LGPL-3',
+    'currency': 'EUR',
+    'price': 75.00,  # Add the price of your module
+    'support': 'Xander.djema@gmail.com',  # Support email
+    'repository': 'https://github.com/djema32/Custom-VAT-Module#18.0',  # Repository URL with the correct branch for Odoo 18.0
 }
